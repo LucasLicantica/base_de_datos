9cb9e80f6e0162a60e630dfd94a503ee8b88683d
lukabazan232 <lukabazan404@gmail.com>
2023-09-07 12:25:43 -0300

    Creando tablas

---
+++ modificaciones/ejercicio2.sql
@@ -0,0 +1,9 @@
+INSERT INTO user(username, password)
+    VALUES ("Lucas", "Nintendo1999"),
+	       ("Nahuel", "Venezuela2124"),
+	       ("Jorge", "Noruega1984");
+	
+INSERT INT post(author_id, title, body)
+    VALUES (1, "River", "Dale campeon"),
+		   (2, "Boca", "El ganador"),
+		   (3, "Barcelona", "El perdedor")
